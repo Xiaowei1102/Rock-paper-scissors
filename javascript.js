01a0ee268fb0bd3859db1c6122b55731bd3b887e
Ryan <43714844+Xiaowei1102@users.noreply.github.com>
2023-07-08 15:57:13 -0500

    Forgot to git during session...Three functions all at once!

---
+++ javascript.js
@@ -0,0 +1,65 @@
+function getComputerChoice() {
+    let arr = ['Rock', 'Paper','Scissors'];
+    return arr[Math.floor(Math.random() * arr.length)];
+}
+//console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSlection) {
+    if (playerSelection.toUpperCase() === 'ROCK' ) {
+        if (computerSlection === 'Scissors') {
+            return 'You win! Rock beats Scissors';
+        } else if (computerSlection === 'Paper') {
+            return 'You lose! Paper beats Rock';
+        }
+        
+    } else if (playerSelection.toUpperCase() === 'SCISSORS') {
+        if (computerSlection === 'Paper') {
+            return 'You win! Scissors beats Paper';
+        } else if (computerSlection === 'Rock') {
+            return 'You lose! ROck beats Scissors';
+        }
+
+    } else if (playerSelection.toUpperCase() === 'PAPER') {
+        if (computerSlection === 'Rock') {
+            return 'You win! Paper beats Rock';
+        } else if (computerSlection === 'Scissors') {
+            return 'You lose! Scissors beats Paper';
+        }
+
+    } 
+    return 'No winner'
+}
+
+/*
+const playerSelection = 'rock';
+const computerSlection= getComputerChoice();
+console.log(computerSlection);
+console.log(playRound(playerSelection,computerSlection));
+*/
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt('Enter your choice');
+        const computerSlection = getComputerChoice();
+        let result = playRound(playerSelection,computerSlection);
+        console.log(result);
+        if (result.slice(4,5) === 'w') {
+            playerScore++;
+        } else if (result.slice(4,5) === 'l') {
+            computerScore++;
+        }
+
+    }
+    if (playerScore > computerScore) {
+        return 'Player won!'
+    } else if (playerScore < computerScore) {
+        return 'Player lose!'
+    } else {
+        return 'Tie game!'
+    }
+
+}
+
+console.log(game());
